a2e10b2535271eadf8647ea6e2a258e389a3ecc9
valerydluski <valerydluski@gmail.com>
2020-10-10 11:18:55 +0300

    feat: add users functional

--- src/resources/users/user.memory.repository.js
+++ src/resources/users/user.memory.repository.js
@@ -1,6 +1,52 @@
+const User = require('./user.model');
+
+const usersDB = [
+  new User({
+    name: 'test',
+    login: 'test',
+    password: 'testPass'
+  }),
+  new User({
+    name: 'test2',
+    login: 'test2',
+    password: 'test2Pass'
+  })
+];
+
 const getAll = async () => {
-  // TODO: mock implementation. should be replaced during task development
-  return [];
+  return usersDB;
 };
 
-module.exports = { getAll };
+const addUser = async (name, login, password) => {
+  try {
+    const newUser = await new User({ name, login, password });
+    usersDB.push(newUser);
+    return newUser;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+const getUserById = async userId => {
+  return usersDB.find(el => el.id === userId);
+};
+
+const updateUser = async (userId, name, login, password) => {
+  const foundUser = usersDB.find(x => x.id === userId);
+  if (foundUser) {
+    foundUser.name = name !== undefined ? name : foundUser.name;
+    foundUser.login = login !== undefined ? login : foundUser.login;
+    foundUser.password = password !== undefined ? password : foundUser.password;
+  }
+  return foundUser;
+};
+
+const deleteUser = async userId => {
+  const foundIdx = usersDB.findIndex(x => x.id === userId);
+  if (foundIdx >= 0) {
+    return usersDB.splice(foundIdx, 1);
+  }
+  return false;
+};
+
+module.exports = { getAll, addUser, getUserById, updateUser, deleteUser };
--- src/resources/users/user.service.js
+++ src/resources/users/user.service.js
@@ -1,5 +1,17 @@
 const usersRepo = require('./user.memory.repository');
 
-const getAll = () => usersRepo.getAll();
+const getAll = async () => await usersRepo.getAll();
 
-module.exports = { getAll };
+const addUser = (name, login, password) => {
+  return usersRepo.addUser(name, login, password);
+};
+
+const getUserById = userId => usersRepo.getUserById(userId);
+
+const updateUser = (userId, name, login, password) => {
+  return usersRepo.updateUser(userId, name, login, password);
+};
+
+const deleteUser = userId => usersRepo.deleteUser(userId);
+
+module.exports = { getAll, addUser, getUserById, updateUser, deleteUser };
