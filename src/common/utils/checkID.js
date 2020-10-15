392972ced0fd388ad898276f6bfcb5d1314f35a4
valerydluski <valerydluski@gmail.com>
2020-10-15 11:57:12 +0300

    feat: add utils checkID and checkExistence

---
+++ src/common/utils/checkID.js
@@ -0,0 +1,14 @@
+const { BadRequest } = require('http-errors');
+
+const checkID = id => {
+  if (
+    !id.match(
+      /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i
+    )
+  ) {
+    throw new BadRequest('ID does not correct');
+  }
+  return id;
+};
+
+module.exports = { checkID };
